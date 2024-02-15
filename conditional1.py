7f4ff882fd370bb0f3e0f9b461491f53d031c135
Yeshey wangdi <02230232.cst@rub.edu.bt>
2024-02-15 14:58:22 +0530

    third commit

---
+++ conditional1.py
@@ -0,0 +1,19 @@
+n=input("please can you tell me your name??")
+print ("Hello!!",n)
+grade= int(input("please can you enter your Annual Marks??"))
+if grade >=90:
+    print("Excellent!! GRADE :A++")
+elif grade >=80:
+    print("VERY GOOD!! GRADE: A")
+elif grade >=70:
+    print("GOOD!! GRADE : B")
+elif grade >=60:
+    print("GRADE : C")
+else:
+    print("GRADE: D")
+    
+print("""NOTICE:
+IT IS ANNOUNCED FOR THE INDIVIDUALS TO CORPORATE WITH THE
+EXAM CELL FOR FUTURE ACTIONS AND ENQUIRES REGARDING THE RESULT WORKSHEET!!
+EXAM CELL: 17******
+EMail:****123.cst@rub.edu.bt""")
