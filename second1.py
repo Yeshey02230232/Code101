9c7ba7e458f13e3d62683e35f10960f0e3d5171a
Yeshey wangdi <02230232.cst@rub.edu.bt>
2024-02-15 10:14:20 +0530

    second commit

---
+++ second1.py
@@ -0,0 +1,12 @@
+print("""Hello Viewers !!!!
+     ,May I know your Good Name?? """)
+n= input("Your name?")
+print("wow Nice to meet You",n)
+
+
+print("  (........)")
+print("  I  o  o  I")
+print ("   (  U  )")
+print ("(````````````)")
+
+print ("well can we be friends??",n)
