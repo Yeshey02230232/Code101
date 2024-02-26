2e3465a5512fdd150e6948e32a35525c96c91166
Yeshey wangdi <02230232.cst@rub.edu.bt>
2024-02-26 13:51:12 +0600

    logical commit

---
+++ logical1.py
@@ -0,0 +1,17 @@
+x=5 
+print(x> 3 and x<10)
+
+y=12
+print(y>10 and y%5==0)
+
+x= 5
+print("your answer is",x<3 or x>10)
+
+y=12
+print("your answer is:",y<10 or y % 2==0)
+
+x=5
+print("your answer is:",not(x>3 and x <10))
+
+y=12
+print("your answer is:",not(y>10 and y%5==0))
