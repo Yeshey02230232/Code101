c636b9ab3943e931ac1b523bda9ada224a2e1217
Yeshey wangdi <02230232.cst@rub.edu.bt>
2024-03-04 16:04:39 +0600

    first commit

--- librarymanager.py
+++ librarymanager.py
@@ -1,33 +1,39 @@
-books_list = []
-authors_set= set()
-books_dict={}
-books_list.append("Python Programming")
-authors_set.add ("John Smith")
-books_dict["PythonProgrsmming"]="John Smith"
+# Initialize empty lists, sets , and dictionary
+books_lists = []
+authors_set = set()
+books_dict = {}
 
-books_list.append("Data Structures and Algorithms")
-authors_set.add ("Jane Doe")
-books_dict["Data structures and algorithms"]="Jane Doe"
+#Add books
+books_lists.append("Python Programming")
+authors_set.add("Jhon Smith")
+books_dict["Python Programming"] = "Jhon Smith"
 
-books_list.append("Machine Learning Basics")
-authors_set.add ("Alice Johnson")
-books_dict["Machine Learniing Basics"]="Alice Johnson"
+books_lists.append("Data Structures and Algorithm")
+authors_set.add("Jane Doe")
+books_dict["Data Structures and Algorithm"] = "Jane Doe"
 
-search=input("Enter the Title of the books to search :")
-if search in books_list:
-    print(f"Book found!! Author:{books_dict[search]}")
+books_lists.append("Machine Leaarning Basics")
+authors_set.add("Alice Jhonson")
+books_dict["Machine Leaarning Basics"] = "Alice Jhonson"
+
+#Search for books
+search_title = input("Enter the title of the book: ")
+if search_title in books_lists: 
+    print(f"Book found! Author: {books_dict[search_title]}")
 else:
-    print("The resource you have entered is not Found!!!")
+    print("Book not found")
 
-print("list of books that we have in the Computer Resources:")
-for books in books_list: 
-    print(books)
-remove_title=input("Enter the title of the book to remove or else enter to Skip!!!:")
-if remove_title in books_list:
+# Display of books
+    print("Lists of books availabe: ")
+    for book in books_lists:
+        print(book)
+# Remove a book
+remove_title = input("Enter the title of the book to be removed: ")
+if remove_title in books_lists: 
     remove_author= books_dict[remove_title]
-    books_list.remove(remove_title)
+    books_lists.remove(remove_title)
     authors_set.remove(remove_author)
     del books_dict[remove_title]
-    print("Book removed Succesfully!!")
+    print('Book removed succesfully!')
 else:
-    print("BOOK NOT FOUND!!")
+    print('Book not found!')
